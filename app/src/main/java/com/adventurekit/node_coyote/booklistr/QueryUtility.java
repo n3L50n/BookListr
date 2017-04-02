edcb178e82d7e1a96ac257665ac8b7dea90b8a6a
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-02 15:09:34 -0700

    Begin parsing JSON response.

--- app/src/main/java/com/adventurekit/node_coyote/booklistr/QueryUtility.java
+++ app/src/main/java/com/adventurekit/node_coyote/booklistr/QueryUtility.java
@@ -1,6 +1,11 @@
 package com.adventurekit.node_coyote.booklistr;
 
+import android.text.TextUtils;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by node_coyote on 4/2/17.
@@ -103,4 +110,56 @@ public final class QueryUtility {
         }
         return output.toString();
     }
+
+    /**
+     *
+     * @param googleBooksJSON
+     * @return
+     */
+    private static List<Book> extractBooks(String googleBooksJSON) {
+
+        // Check if JSON is empty and return if so
+        if (TextUtils.isEmpty(googleBooksJSON)){
+            return null;
+        }
+
+        // Create an empty array to hold the books
+        List<Book> books = new ArrayList<>();
+
+        // Attempt to parse the JSON. Throw an exception if there is a problem
+        try {
+            // Begin with root JSON object
+            JSONObject response = new JSONObject(googleBooksJSON);
+
+            // Step into JSON array "items"
+            JSONArray items = response.getJSONArray("items");
+
+            // Cycle through items within the array to create new Books
+            for (int i = 0; i < items.length(); i++ ){
+
+                // Get a Book
+                JSONObject book = items.getJSONObject(i);
+
+                // Get the title of a book
+                String title = book.getString("title");
+
+                // Get the authors array of a book
+                JSONArray authors = book.getJSONArray("authors");
+
+                // Cycle through Array of authors to get authors
+                for (int j = 0; j < authors.length(); j++){
+                    String author = authors.getString(j);
+                }
+
+                Book newBook = new Book(title, author);
+                books.add(newBook);
+
+            }
+
+        } catch (JSONException e){
+            Log.e(LOG_TAG, "Problem parsing JSON results", e);
+        }
+        return books;
+    }
+
 }
