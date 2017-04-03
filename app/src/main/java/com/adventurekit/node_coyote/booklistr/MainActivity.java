79a585d3c34d999d01e5f43bc6a72d512759559a
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-03 07:55:00 -0700

    Fill out Loader methods.

--- app/src/main/java/com/adventurekit/node_coyote/booklistr/MainActivity.java
+++ app/src/main/java/com/adventurekit/node_coyote/booklistr/MainActivity.java
@@ -6,14 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
+
+    public static final String GOOGLE_BOOKS_REQUEST_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=1";
 
     // The base url to build a query upon
-    public static final String GOOGLE_BOOKS_REQUEST_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+    //public static final String GOOGLE_BOOKS_REQUEST_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
+    // TODO do I need an API key?
+    // API KEY
     public static final String API_KEY = "AIzaSyCpdjPykoL3HtKyceMX5Jf8nd-q0f92iMs";
 
     // Variable to store the adapter
@@ -24,40 +30,53 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Find a reference to the {@link ListView}
+        ListView bookListView = (ListView) findViewById(R.id.book_list);
+
+        //Create a new {@link ArrayAdapter} of Books
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
-    }
-    /**
-     *
-     * @param baseUrl
-     * @return
-     */
-    public String getRequestUrl(String baseUrl){
 
+        bookListView.setAdapter(mAdapter);
+
+    }
+
+
+//    public String getRequestUrl(String baseUrl) {
+//
 //        // Begin with Google Books basic url
-        baseUrl = GOOGLE_BOOKS_REQUEST_BASE_URL;
-
-        //TODO for each word entered in search, split between spaces
-        
+//        baseUrl = GOOGLE_BOOKS_REQUEST_BASE_URL;
+//
+//        //TODO for each word entered in search, split between spaces
+//
 //        // Grab Search View
-        EditText searchInput = (EditText) findViewById(R.id.search_bar);
-        String searchQuery = searchInput.getText().toString();
-        String requestUrl = baseUrl + searchQuery + "&key=" + API_KEY;
+//        EditText searchInput = (EditText) findViewById(R.id.search_bar);
+//        String searchQuery = searchInput.getText().toString();
+//        String requestUrl = baseUrl + searchQuery + "&key=" + API_KEY;
+//
+//        return requestUrl;
+//    }
 
-        return requestUrl;
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        return new BookLoader(MainActivity.this, GOOGLE_BOOKS_REQUEST_BASE_URL);
     }
 
     @Override
-    public Loader onCreateLoader(int id, Bundle args) {
-        return null;
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> bookData) {
+
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        mAdapter.clear();
+
+        if (bookData != null && !bookData.isEmpty()){
+            mAdapter.addAll(bookData);
+        }
     }
 
     @Override
-    public void onLoadFinished(Loader loader, Object data) {
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader loader) {
-
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        mAdapter.clear();
     }
 }
